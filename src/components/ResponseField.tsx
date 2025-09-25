e0b7ed0c31b7abacaf9a666a6932971f6efe4063
Bhupesh Dahiya <149587292+basesixtyfour@users.noreply.github.com>
2025-09-25 13:18:15 +0530

    create boilerplate frontend

---
+++ src/components/ResponseField.tsx
@@ -0,0 +1,7 @@
+export default function ResponseField() {
+  return (
+    <div className="w-1/2 min-w-32 border-2 border-gray-300 rounded-md shrink-0">
+      Hii! I am the response field.
+    </div>
+  );
+}
