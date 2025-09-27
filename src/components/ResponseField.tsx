1d522347a75104ffa8cf46e96d215a85925307c6
Bhupesh Dahiya <149587292+basesixtyfour@users.noreply.github.com>
2025-09-27 21:22:27 +0530

    feature: syntax highlighting

--- src/components/ResponseField.tsx
+++ src/components/ResponseField.tsx
@@ -1,12 +1,37 @@
 import { Card, CardContent } from "@/components/ui/card";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import remarkGfm from "remark-gfm";
 
 export default function ResponseField({ content }: { content: string }) {
   return (
     <Card className="rounded-md dark:bg-[#2d2d30] text-white border-2 border-gray-300 shrink-0">
       <CardContent>
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code(props) {
+              const { children, className, node, ref, ...rest } = props
+              const match = /language-(\w+)/.exec(className || '')
+              return match ? (
+                <SyntaxHighlighter
+                  {...rest}
+                  PreTag="div"
+                  children={String(children).replace(/\n$/, '')}
+                  language={match[1]}
+                  style={theme}
+                />
+              ) : (
+                <code {...rest} className={className}>
+                  {children}
+                </code>
+              )
+            }
+          }}
+        >
+          {content}
+        </ReactMarkdown>
       </CardContent>
     </Card>
   );
